4aa1c19c3318e707c5453ee932865740c52d0fa5
IvannaGumeniuk <vany93ok@gmail.com>
2022-06-13 23:23:21 +0300

    hw2 start

---
+++ src/js/02-timer.js
@@ -0,0 +1,24 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+// const input = document.querySelector(input[type = "text"]);
+// const options = {
+//     altInput: true,
+//     altFormat: "F j, Y",
+//     dateFormat: "Y-m-d",
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
+
+// flatpickr(input, options);
+    
+// {
+//     altInput: true,
+//     altFormat: "F j, Y",
+//     dateFormat: "Y-m-d",
+// }
