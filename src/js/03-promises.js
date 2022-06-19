d0cc47b6cbf6ec6df243989b1bcb3058e03b7688
IvannaGumeniuk <vany93ok@gmail.com>
2022-06-19 22:31:16 +0300

    hw-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+const firstDelay = document.querySelector('input[name="delay"]');
+const delaysStep = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    
+    const shouldResolve = Math.random() > 0.3;
+    
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+ 
+  })
 }
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  let delay = Number(firstDelay.value);
+  let position = Number(amount.value);
+  let step = Number(delaysStep.value);
+
+  	for (let i = 1; i <= position; i++) {
+			createPromise(i, delay)
+			.then(({ position, delay }) => {
+				Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+			})
+			.catch(({ position, delay }) => {
+				Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+			});
+  			delay = delay + step;	
+	}
+})
